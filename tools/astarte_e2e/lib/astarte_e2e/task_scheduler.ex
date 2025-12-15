09c5af6a90efc29dd58b106feff4f3973b0e2a47
Francesco Noacco <francesco.noacco@secomind.com>
2025-12-15 11:14:05 +0100

    remove device trigger check
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- tools/astarte_e2e/lib/astarte_e2e/task_scheduler.ex
+++ tools/astarte_e2e/lib/astarte_e2e/task_scheduler.ex
@@ -45,7 +45,7 @@ defmodule AstarteE2E.TaskScheduler do
       AstarteE2E.AmqpDataTrigger,
       AstarteE2E.AmqpDeviceTrigger,
       AstarteE2E.DataTrigger,
-      AstarteE2E.DeviceTrigger,
+      # AstarteE2E.DeviceTrigger,
       AstarteE2E.DeviceDeletion,
       AstarteE2E.VolatileTriggerRoundtrip.Executor
     ]
