7d577b65efab7b30093817e20a86bf949df40b14
Francesco Noacco <francesco.noacco@secomind.com>
2025-12-15 11:12:57 +0100

    remove device deletion check
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- tools/astarte_e2e/lib/astarte_e2e/task_scheduler.ex
+++ tools/astarte_e2e/lib/astarte_e2e/task_scheduler.ex
@@ -46,7 +46,7 @@ defmodule AstarteE2E.TaskScheduler do
       AstarteE2E.AmqpDeviceTrigger,
       AstarteE2E.DataTrigger,
       AstarteE2E.DeviceTrigger,
-      AstarteE2E.DeviceDeletion,
+      # AstarteE2E.DeviceDeletion,
       AstarteE2E.VolatileTriggerRoundtrip.Executor
     ]
 
